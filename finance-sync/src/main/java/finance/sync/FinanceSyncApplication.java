58fbd7a8438cb21d90f94fcd3219375d197706f2
ElielRegiani <elielregiani@gmail.com>
2024-10-30 09:29:36 -0300

    First Commit

---
+++ finance-sync/src/main/java/finance/sync/FinanceSyncApplication.java
@@ -0,0 +1,13 @@
+package finance.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSyncApplication.class, args);
+	}
+
+}
---
+++ finance-sync/src/test/java/finance/sync/FinanceSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package finance.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
